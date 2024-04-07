5c8539b371599d654ff81b0091d81060c9f4c475
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-04-08 01:57:21 +0200

    chore(Root): Migrate from Vite to Astro
    - Transitioned the project from Vite to Astro for improved performance and build optimization.

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['InterLatin', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
